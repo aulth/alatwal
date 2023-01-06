271080e6bbaeac49677c35ac51c3b262437b2c5e
aulth <mohdusman.you@gmail.com>
2023-01-07 02:04:52 +0530

    tour.js page Tourservice not resolved fixed

--- pages/tour.js
+++ pages/tour.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import LandingPage from '../components/LandingPage'
 import Navbar from '../components/Navbar'
-import Tourservice from '../components/Tourservice'
 import Carousel from '../components/Carousel'
 const Tour = () => {
   return (
     <>
     <Navbar/>
-    <Tourservice/>
     <Carousel images={['https://source.unsplash.com/random/?Desert', 'https://source.unsplash.com/random/?Mountain', 'https://source.unsplash.com/random/?Nature']} />
     </>
   )
