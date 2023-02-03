efc6522f864000ada1590b1f98cd126812c7df84
aulth <mohdusman.you@gmail.com>
2023-02-03 18:20:02 +0530

    new tripadvisor widget added

--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -6,8 +6,11 @@ const Testimonial = () => {
       <div>
         <div className="min-w-screen  flex items-center justify-center">
           <div className="w-full bg-white  px-5 py-16 md:py-24 text-gray-800">
+            {/* <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+            <div className="elfsight-app-54e7b8ce-f26a-418a-ac47-df6b3d101bb8"></div> */}
+            {/* 03-02-2023 */}
             <script src="https://apps.elfsight.com/p/platform.js" defer></script>
-            <div className="elfsight-app-54e7b8ce-f26a-418a-ac47-df6b3d101bb8"></div>
+            <div class="elfsight-app-60a4fb15-fe5d-4fab-ab20-143b4a65721e"></div>
             {/* <script src="https://widget.trustmary.com/Y8oa9ta6K" async></script> */}
             {/* <script src="https://widget.trustmary.com/Y8oa9ta6K" async></script> */}
           </div>
